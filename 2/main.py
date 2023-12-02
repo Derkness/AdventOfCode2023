ef8a58e9a10fbef08b9ce8eb61cf6cf02f0206e9
Derkness <tlpapae@gmail.com>
2023-12-02 16:12:34 +1030

    Clean up some of the code's colour repetition

--- 2/main.py
+++ 2/main.py
@@ -13,6 +13,13 @@ def part_1():
     numRed = int(input("How many red? "))
     numGreen = int(input("How many green? "))
     numBlue = int(input("How many blue? "))
+    
+    colourPairs = [
+        ["red", numRed],
+        ["green", numGreen],
+        ["blue", numBlue],
+    ]
+    
     count = 0
     lines = get_input()
     for line in lines:
@@ -24,46 +31,45 @@ def part_1():
         for gameStage in gameStages:
             colours = [x.strip() for x in gameStage.split(',')]
             for colour in colours:
-                if "red" in colour:
-                    if numRed < get_val(colour, "red"):
-                        possible = False
-                        break
-                if "blue" in colour:
-                    if numBlue < get_val(colour, "blue"):
-                        possible = False
-                        break
-                if "green" in colour:
-                    if numGreen < get_val(colour, "green"):
-                        possible = False
-                        break
+                for colourPair in colourPairs:
+                    if colourPair[0] in colour:
+                        if colourPair[1] < get_val(colour, colourPair[0]):
+                            possible = False
+                            break
+                if possible == False:
+                    break
             if possible == False:
                 break
         if (possible):
             count += gameId            
     print(count)
-    
+
 def part_2():
     count = 0
     lines = get_input()
     for line in lines:
-        [gameName, gameContents] = line.split(':')
-        gameId = int(gameName.replace("Game ",""))
+        [_, gameContents] = line.split(':')
         
-        maxRed = 0
-        maxGreen = 0
-        maxBlue = 0
+        colourMaximums = [
+            ["red", 0],
+            ["green", 0],
+            ["blue", 0],
+        ]
         
         gameStages = [x.strip() for x in gameContents.split(';')]
         for gameStage in gameStages:
             colours = [x.strip() for x in gameStage.split(',')]
             for colour in colours:
-                if "red" in colour:
-                    maxRed = max(maxRed, get_val(colour, "red"))
-                if "green" in colour:
-                    maxGreen = max(maxGreen, get_val(colour, "green"))
-                if "blue" in colour:
-                    maxBlue = max(maxBlue, get_val(colour, "blue"))
-        count += maxRed * maxGreen * maxBlue
+                for colourMaximum in colourMaximums:
+                    if colourMaximum[0] in colour:
+                        colourMaximum[1] = max(colourMaximum[1], get_val(colour, colourMaximum[0]))
+
+        power = 1
+        for colourMaximum in colourMaximums:
+            power *= colourMaximum[1]
+            
+        count += power
+
     print(count)
     
 if __name__ == "__main__":
