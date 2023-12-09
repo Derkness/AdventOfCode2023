4f7681f26404f03f30baf33acd916a7fce1fc9d6
Derkness <tlpapae@gmail.com>
2023-12-10 00:12:11 +1030

    Day 9 complete
    Was weirdly easy? (Break looks long but I was having a Christmas dinner!

--- 9/main.py
+++ 9/main.py
@@ -14,14 +14,41 @@ def extrapolate_value(line):
     nest = [line]
     while not (len(set(nest[-1])) == 1 and 0 in nest[-1]):
         nest.append(find_pattern(nest[-1]))
-    print(nest)
-    return 2
+    nest.reverse()
+    increment = None
+    for level in nest:
+        if (increment is not None):
+            level.append(level[-1] + increment)
+        increment = level[-1]
+        # print(increment)
+    return nest[-1].pop()
         
 def part_1():
     lines = get_input()
     count = 0
     for line in lines:
         count += extrapolate_value(line)
+    print(count)
+
+
+def extrapolate_previous_value(line):
+    nest = [line]
+    while not (len(set(nest[-1])) == 1 and 0 in nest[-1]):
+        nest.append(find_pattern(nest[-1]))
+    nest.reverse()
+    increment = None
+    for level in nest:
+        if (increment is not None):
+            level.insert(0, level[0] - increment)
+        increment = level[0]
+    return nest[-1][0]
+        
+def part_2():
+    lines = get_input()
+    count = 0
+    for line in lines:
+        count += extrapolate_previous_value(line)
+    print(count)
 
 if __name__ == "__main__":
-    part_1()
+    part_2()
