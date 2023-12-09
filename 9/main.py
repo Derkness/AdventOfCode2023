441a34f93b08717cd473201283b618b2466c256d
Derkness <tlpapae@gmail.com>
2023-12-09 18:37:24 +1030

    started (finds pattern well)

---
+++ 9/main.py
@@ -0,0 +1,27 @@
+def get_input():
+    lines=[]
+    with open("input.txt", "r") as file:
+        for line in file:
+            strippedLine = line.strip()
+            if (len(strippedLine) > 0):
+                lines.append([int(x) for x in strippedLine.split()])
+    return lines
+
+def find_pattern(line):
+    return [line[i+1] - line[i] for i in range(len(line) - 1)]
+
+def extrapolate_value(line):
+    nest = [line]
+    while not (len(set(nest[-1])) == 1 and 0 in nest[-1]):
+        nest.append(find_pattern(nest[-1]))
+    print(nest)
+    return 2
+        
+def part_1():
+    lines = get_input()
+    count = 0
+    for line in lines:
+        count += extrapolate_value(line)
+
+if __name__ == "__main__":
+    part_1()
